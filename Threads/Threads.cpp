33b71a6f3e4f311c5d5be34632dc8144bd4d73bf
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2020-05-15 18:14:18 -0700

    create Thread Lab

---
+++ Threads/Threads.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <fstream>
+using namespace std;
+
+vector<int> createNums(int size);
+void bubbleSort(vector<int> &nums);
+void insertionSort();
+void quickSort();
+void readNums();
+void writeNums();
+
+int main(int argc, char* argv[])
+{
+
+}
+
+void bubbleSort(vector<int> &nums)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            
+        }
+        
+    }
+}
