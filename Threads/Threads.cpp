f323e793dcff446744583cf5cdc08c78b4effb2c
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2020-05-19 23:22:25 -0700

    add file locking and user input

--- Threads/Threads.cpp
+++ Threads/Threads.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <time.h>
+#include <ctime>
 #include <atomic>
 #include <pthread.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/file.h>
 using namespace std;
 
 void bubbleSort(vector<int> &nums);
@@ -12,28 +14,76 @@ void insertionSort(vector<int> &nums);
 void quickSort(vector<int> &nums, int low, int high);
 int partition(std::vector<int> &nums, int low, int high);
 int getRandom(int low, int high);
+bool validateInput(char* input);
 vector<int> readNums(int start, int end);
-void writeNums(vector<int> &nums, int index, int size);
+void writeNums(vector<int> &nums, int index);
 void *threadCheck(void *threadId);
 void *threadSort(void *threadId);
-void threadmanager();
+void threadLauncher();
 
-atomic<int> mtx;
 atomic<bool> sorted;
 atomic<bool> check;
 int size;
-int threadCount;
-
 
 int main(int argc, char* argv[])
 {
-    // validate command line input
-    size = 10;
+    if (argc < 2)
+    {
+        cout << "You need to provide how many numbers to sort" << endl;
+    }
+    else if (argc > 2)
+    {
+        cout << "You have provided too many parameters" << endl;
+    }
+    else if (validateInput(argv[1]))
+    {
+        threadLauncher();
+    }
+    else
+    {
+        cout << "You need to provide an integer value that is greater than 1 to start sorting" << endl;
+    }
+    
+    return 0;
+}
 
-    // ask for thread count and validate
-    threadCount = 5;
+bool validateInput(char* input)
+{
+    string str(input);
 
-    // fill file with random numbers
+    for (int i = 0; i < str.length(); i++) {
+        if (!isdigit(str[i]))
+        {
+            return false;
+        }
+    }
+    size = atoi(input);
+
+    if (size < 2)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void threadLauncher()
+{
+    sorted = false;
+    check = true;
+    int threadCount = 0;
+    
+    cout << "How many threads do you want to sort the file?" << endl;
+    cin >> threadCount;
+    while (cin.fail() || threadCount < 1)
+    {
+        cout << "You need to input an integer greater than 0 to start" << endl;
+        cin.clear();
+        cin.ignore(256, '\n');
+        cin >> threadCount;
+    }
+    threadCount += 1;
+
     srand(time(NULL));
     fstream file("Sorted.txt", fstream::out);
     for (int i = 0; i < size; i++)
@@ -42,11 +92,22 @@ int main(int argc, char* argv[])
     }
     file.close();
 
-    // create threads
-    mtx = 1;
-    sorted = false;
-    check = true;
-
+    string str;
+    vector<int> nums = readNums(0, size);
+    cout << "File before sorting:" << endl;
+    for (int i = 0; i < nums.size(); i++) 
+    {
+        if (i == nums.size() - 1)
+        {
+            str.append(to_string(nums[i]) + "\n");
+        }
+        else
+        {
+            str.append(to_string(nums[i]) + ", ");
+        }
+    }
+    cout << str << endl;
+    
     pthread_t threads[threadCount];
     for (int i = 0; i < threadCount; i++)
     {
@@ -73,7 +134,21 @@ int main(int argc, char* argv[])
         pthread_join(threads[i], NULL);
     }
 
-    return 0;
+    nums = readNums(0, size);
+    str = "";
+    cout << "File after sorting:" << endl;
+    for (int i = 0; i < nums.size(); i++) 
+    {
+        if (i == nums.size() - 1)
+        {
+            str.append(to_string(nums[i]) + "\n");
+        }
+        else
+        {
+            str.append(to_string(nums[i]) + ", ");
+        }
+    }
+    cout << str << endl;
 }
 
 void bubbleSort(vector<int> &nums)
@@ -126,7 +201,8 @@ int partition(std::vector<int> &nums, int low, int high)
     int pivot = nums[high];
     int i = (low - 1);
 
-    for(int j = low; j <= high - 1; j++){
+    for(int j = low; j <= high - 1; j++)
+    {
         if(nums[j] < pivot){
             i++;
             iter_swap(nums.begin() + i, nums.begin() + j);
@@ -182,12 +258,19 @@ vector<int> readNums(int start, int end)
 
 void *threadCheck(void *threadId)
 {
+    int id = (int) (long) threadId;
     while (!sorted)
     {
         if (check)
         {
-            cout << "checking" << endl;
+            int fd;
+            fd = open("Sorted.txt", O_RDWR);
+            flock(fd, LOCK_EX);
+
             vector<int> nums = readNums(0, size);
+            flock(fd, LOCK_UN);
+            close(fd);
+
             int count = 0;
 
             if (nums.size() == 1)
@@ -198,17 +281,18 @@ void *threadCheck(void *threadId)
             {
                 for (int i = 1; i < nums.size(); i++)
                 {
-                    if (nums.at(i) > nums.at(i-1))
+                    if (nums.at(i) >= nums.at(i-1))
                     {
                         count++;
                     }
                 }
 
-                if (count == size)
+                if (count == size - 1)
                 {
                     sorted = true;
                 }
             }
+
             check = false;
         }
     }
@@ -222,41 +306,55 @@ void *threadSort(void *threadId)
 
     while (!sorted)
     {
-        if (mtx == id)
+        int i = getRandom(0, size - 2);
+        int j = getRandom(i + 1, size - 1);
+
+        // lock the file
+        int fd;
+        fd = open("Sorted.txt", O_RDWR);
+        flock(fd, LOCK_EX);
+
+        vector<int> nums = readNums(i, j);
+
+        string method;
+        switch (getRandom(0, 2))
         {
-            int i = getRandom(0, size - 2);
-            int j = getRandom(i + 1, size - 1);
-            cout << i << " " << j << endl;
+            case 0:
+                quickSort(nums, 0, nums.size() - 1);
+                method = "QuickSort";
+                break;
+            case 1:
+                insertionSort(nums);
+                method = "Insertion Sort";
+                break;
+            case 2:
+                bubbleSort(nums);
+                method = "Bubble Sort";
+                break;
+            default:
+                break;
+        }
 
-            vector<int> nums = readNums(i, j);
+        writeNums(nums, i);
 
-            switch (getRandom(0, 2))
+        string output = "Thread " + to_string(id) + " used " + method + " on "  + to_string(i) + " - " + to_string(j) + ":\n";
+        for (int i = 0; i < nums.size(); i++) {
+            if (i == nums.size() - 1)
             {
-                case 0:
-                    insertionSort(nums);
-                    break;
-                case 1:
-                    insertionSort(nums);
-                    break;
-                case 2:
-                    bubbleSort(nums);
-                    break;
-                default:
-                    break;
-            }
-
-            writeNums(nums, i);
-
-            if (mtx == threadCount - 1)
-            {
-                mtx = 1;
+                output.append(to_string(nums[i]) + "\n");
             }
             else
             {
-                mtx++;
+                output.append(to_string(nums[i]) + ", ");
             }
-            check = true;
         }
+        cout << output << endl;
+
+        //unlock file
+        flock(fd, LOCK_UN);
+        close(fd);
+        check = true;
+        sleep(.01);
     }
 
     return (void*)0;
